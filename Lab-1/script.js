c279d1b08d9eb7d62a484743ef0a74d0252d4198
Kewin-Bieganski <kewinbieganski@gmail.com>
2023-03-24 11:44:27 +0100

    Zmiana drogi zycia.

--- Lab-1/script.js
+++ Lab-1/script.js
@@ -1,6 +1,6 @@
 addEventListener("DOMContentLoaded", () => {
-    var changeCalendarButton = document.querySelector(".title");
-    let calendarContainer = document.querySelector(".container");
+    const changeCalendarButton = document.querySelector(".title");
+    const calendarContainer = document.querySelector(".container");
     changeCalendarButton.addEventListener("click", () => {
         calendarContainer.classList.toggle("flat");
     });
